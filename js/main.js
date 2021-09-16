202c7e61847e5f3c63d8ab04a54f3f5a720ade8c
Matveyx25 <matveyx25@gmail.com>
2021-09-16 09:35:49 +0400

    Debug

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var hoverDistort = new hoverEffect({
     parent: document.querySelector('.elips'),
     intensity: 0.5,
-    image1: './img/XgeZu2jBaVI.jpg',
+    image1: '/img/XgeZu2jBaVI.jpg',
     image2: './img/XgeZu2jBaVI.jpg',
     displacementImage: './img/4.png'
 });
