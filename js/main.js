444f5901ba7e37ca97b10724e2504f4dcb0948f1
Matveyx25 <matveyx25@gmail.com>
2021-09-16 12:27:16 +0400

    Animations home-page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
+gsap.registerPlugin(ScrollTrigger);
+
 var hoverDistort = new hoverEffect({
     parent: document.querySelector('.elips'),
     intensity: 0.5,
     image1: './img/XgeZu2jBaVI.jpg',
     image2: './img/XgeZu2jBaVI.jpg',
-    displacementImage: './img/4.png'
+    displacementImage: './img/8.png'
 });
 
+$(window).ready(         
+    function () {
+    anime.timeline({loop: false})
+    .add({
+        targets: '.title__home',
+        translateX: (el, i) => [Math.pow(-1,i)*150,0],
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: (el, i) => 400 + (1000 * i),
+        delay: (el, i) => 200 * (i + 1)
+    })
+    }
+);
+
+
+gsap.from(".scrollTrigger", {
+    scrollTrigger: '.scrollTrigger',
+    y: 200,
+    opacity: 0,
+    duration: 0.5, 
+    ease: 'easeInOut'
+});
