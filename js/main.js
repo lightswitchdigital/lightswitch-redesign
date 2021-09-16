631846f24c7f20e8a89e9af46cc9836da4d6b360
Matveyx25 <matveyx25@gmail.com>
2021-09-16 09:32:32 +0400

    img paths

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var hoverDistort = new hoverEffect({
     parent: document.querySelector('.elips'),
     intensity: 0.5,
-    image1: './../img/XgeZu2jBaVI.jpg',
-    image2: './../img/XgeZu2jBaVI.jpg',
-    displacementImage: './../img/4.png'
+    image1: './img/XgeZu2jBaVI.jpg',
+    image2: './img/XgeZu2jBaVI.jpg',
+    displacementImage: './img/4.png'
 });
 
