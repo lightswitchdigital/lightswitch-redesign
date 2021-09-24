26ac658effade30d2965cad556c773d9507ce74e
Matveyx25 <matveyx25@gmail.com>
2021-09-24 21:52:07 +0400

    Home page adaptive

--- js/main.js
+++ js/main.js
@@ -9,40 +9,15 @@ var hoverDistort = new hoverEffect({
 });
 
 $(window).ready(function () {
-    // anime.timeline({loop: false})
-    //     .add({
-    //         targets: '.title__home',
-    //         translateX: (el, i) => [Math.pow(-1,i)*150,0],
-    //         opacity: [0,1],
-    //         easing: "easeOutExpo",
-    //         duration: (el, i) => 400 + (1000 * i),
-    //         delay: (el, i) => 200 * (i + 1)
-    //     })
-
-    const firstRow = document.querySelector('.title__first-row .letters');
-    firstRow.innerHTML = firstRow.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-    const secondRow = document.querySelector('.title__second-row .letters');
-    secondRow.innerHTML = secondRow.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-    const thirdRow = document.querySelector('.title__third-row .letters');
-    thirdRow.innerHTML = thirdRow.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-    const fourthRow = document.querySelector('.title__fourth-row .letters');
-    fourthRow.innerHTML = fourthRow.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-    anime.timeline({loop: true})
+    anime.timeline({loop: false})
         .add({
-            targets: '.letter',
-            translateY: ["1.1em", 0],
-            translateX: ["0.55em", 0],
-            translateZ: 0,
-            rotateZ: [180, 0],
-            duration: 300,
+            targets: '.title__home',
+            translateX: (el, i) => [Math.pow(-1,i)*150,0],
+            opacity: [0,1],
             easing: "easeOutExpo",
-            delay: (el, i) => 50 * i
-        });
-
+            duration: (el, i) => 400 + (1000 * i),
+            delay: (el, i) => 200 * (i + 1)
+        })
 
 });
 
@@ -54,3 +29,7 @@ gsap.from(".scrollTrigger", {
     duration: 0.5, 
     ease: 'easeInOut'
 });
+
+document.querySelector('.menu-burger').addEventListener('click', () => {
+    document.querySelector('.mobile-header').classList.toggle('active')
+})
