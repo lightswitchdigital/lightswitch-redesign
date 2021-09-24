125dba7c2bf9423b918f9f9d222f5ae2280682d2
Matveyx25 <matveyx25@gmail.com>
2021-09-24 22:06:49 +0400

    Price adaptive

--- js/main.js
+++ js/main.js
@@ -1,34 +1,38 @@
 gsap.registerPlugin(ScrollTrigger);
 
-var hoverDistort = new hoverEffect({
-    parent: document.querySelector('.elips'),
-    intensity: 0.6,
-    image1: './img/XgeZu2jBaVI.jpg',
-    image2: './img/XgeZu2jBaVI.jpg',
-    displacementImage: './img/8.png'
-});
+if(document.querySelector('.elips')){
+    var hoverDistort = new hoverEffect({
+        parent: document.querySelector('.elips'),
+        intensity: 0.6,
+        image1: './img/XgeZu2jBaVI.jpg',
+        image2: './img/XgeZu2jBaVI.jpg',
+        displacementImage: './img/8.png'
+    });
+}
 
-$(window).ready(function () {
-    anime.timeline({loop: false})
-        .add({
-            targets: '.title__home',
-            translateX: (el, i) => [Math.pow(-1,i)*150,0],
-            opacity: [0,1],
-            easing: "easeOutExpo",
-            duration: (el, i) => 400 + (1000 * i),
-            delay: (el, i) => 200 * (i + 1)
-        })
+if(document.querySelector('.title__home')){
+    $(window).ready(function () {
+        anime.timeline({loop: false})
+            .add({
+                targets: '.title__home',
+                translateX: (el, i) => [Math.pow(-1,i)*150,0],
+                opacity: [0,1],
+                easing: "easeOutExpo",
+                duration: (el, i) => 400 + (1000 * i),
+                delay: (el, i) => 200 * (i + 1)
+            })
+    });
+}
 
-});
-
-
-gsap.from(".scrollTrigger", {
-    scrollTrigger: '.scrollTrigger',
-    y: 200,
-    opacity: 0,
-    duration: 0.5, 
-    ease: 'easeInOut'
-});
+if(document.querySelector('.scrollTrigger')){
+    gsap.from(".scrollTrigger", {
+        scrollTrigger: '.scrollTrigger',
+        y: 200,
+        opacity: 0,
+        duration: 0.5, 
+        ease: 'easeInOut'
+    });
+}
 
 document.querySelector('.menu-burger').addEventListener('click', () => {
     document.querySelector('.mobile-header').classList.toggle('active')
